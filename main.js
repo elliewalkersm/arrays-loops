a663e68396a59e64fc4a01a187866e1ae9a5f8da
elliewalkersm <elliewalkersm@gmail.com>
2021-01-19 20:39:02 -0600

    pop,push

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("Did it!");
+// console.log("Did it!");
+// ****ARRAY METHODS***
+
+const array1 = ['cow', 'dog', 'cat'];
+const string1 = (array1.join(', '));
+// // console.log(string1);
+
+// const string1ToArray = string1.split(', ');
+// console.log(string1ToArray);
+
+// ***PUSH AND POP***
+// array1.pop();
+// console.log(array1);
+// array1.push('cat');
+// console.log(array1);
+
+// ***UNSHIFT AND SHIFT***
+
+array1.shift();
+console.log(array1);
+array1.unshift('cat');
+console.log(array1);
